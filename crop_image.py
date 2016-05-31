e7b2a90f86986bb71dd9aa49cb4f4a4d43cab9dd
Jakub Dvorak <kdvorak12@seznam.cz>
2016-05-31 22:57:35 +0200

    Pridani funkce CROP

--- crop_image.py
+++ crop_image.py
@@ -1,6 +1,13 @@
 import sys
 from os import path
+from PIL import Image
 
+
+def crop_image(image_path):
+    input_image = Image.open(image_path)
+    coordinates = (10, 20, 10 + 60, 20 + 50)
+    output_image = input_image.crop(coordinates)
+    return output_image
 
 def print_help():
     print("usage: <input image> <output_image>")
@@ -12,6 +19,8 @@ def main():
         sys.exit(1)
     if not path.exists(sys.argv[1]):
         raise FileExistsError("File %s does not exists" % sys.argv[1])
+    output_image = crop_image(sys.argv[1])
+    output_image.save(sys.argv[2])
 
 
 if __name__ == "__main__":
