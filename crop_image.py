6f2f68c5e2a64b5a2c395f4ba9378c2c11cd3ca1
kuba <dvorakj31@gmail.com>
2016-06-03 21:43:55 +0200

    Dokoncena funkce add_metadata

--- crop_image.py
+++ crop_image.py
@@ -1,33 +1,48 @@
+#!/usr/bin/env python3
+
 import sys
 from os import path
 from PIL import Image
 
 
 def add_metadata(output_image, metadata):
-    print("something")
+    meta = []
+    with open(metadata, 'rb') as input_metadata:
+        for byte in input_metadata:
+            meta.append(byte)
+    with open(output_image, 'ab') as output:
+        output.write(b'\0')
+        for byte in meta:
+            output.write(byte)
 
 
 def crop_image(image_path):
     with Image.open(image_path) as input_image:
         width, height = input_image.size
-        output_image = input_image.crop((0, 0, width/2, height))
+        output_image = input_image.crop((0, 0, width//2, height))
     return output_image
 
 
 def print_help():
-    print("--help, -h                                               Prints this information")
-    print("--add, -a <input image> <output image> <metadata file>   Adds metadata to image")
-    print("--extract, -e <input image> <output metadata file>       Extracts metadata from image")
+    print("--help, -h                                                     Prints this information")
+    print("--add, -a <input image> <output image> <input metadata file>   Adds metadata to image")
+    print("--extract, -e <input image> <output metadata file>             Extracts metadata from image")
 
 
 def main():
-    if len(sys.argv) != 3 or sys.argv[1] == "-h" or sys.argv[1] == "--help":
+    if len(sys.argv) <= 3 or sys.argv[1] == "-h" or sys.argv[1] == "--help":
         print_help()
         sys.exit(1)
-    if not path.exists(sys.argv[1]):
-        raise FileExistsError("File %s does not exists" % sys.argv[1])
-    output_image = crop_image(sys.argv[1])
-    output_image.save(sys.argv[2])
+    if not path.exists(sys.argv[2]):
+        raise FileExistsError("File %s does not exists" % sys.argv[2])
+    if sys.argv[1] == "--add" or sys.argv[1] == "-a":
+        if len(sys.argv) != 5:
+            print_help()
+        if not path.exists(sys.argv[4]):
+            raise FileExistsError("File %s does not exists" % sys.argv[4])
+        output_image = crop_image(sys.argv[2])
+        output_image.save(sys.argv[3])
+        add_metadata(sys.argv[3], sys.argv[4])
 
 
 if __name__ == "__main__":
